8cd21f5240d285ab25f2fdfadf12fd9968ce92ac
pr-Mendes <jhonatanmendes1471@gmail.com>
2024-10-31 20:24:03 -0300

    commit aula 1

---
+++ trabalho.sql
@@ -0,0 +1,13 @@
+/* CREATE TABLE hotel.clientes
+(
+	id_clientes INT AUTO_INCREMENT,
+    nome varchar(100),
+    email varchar(50),
+    telefone int(16),
+    documento int(20),
+    PRIMARY KEY(id_clientes)
+); */
+
+/* ALTER TABLE hotel.clientes
+MODIFY telefone VARCHAR(16),
+MODIFY documento VARCHAR(20); */
